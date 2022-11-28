58fdeae0eac22fd5f4fa1e5277c4209ce69e8371
Agent Hacker <43415449+AgentHackerYT@users.noreply.github.com>
2022-11-28 16:01:03 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,83 @@
+try{
+const Quickdb = require('quick.db')
+
+const db = new Quickdb.QuickDB()
+
+const express = require('express')
+
+const app = express()
+
+app.listen(8080, () =>console.log("http://localhost:8080/"))
+
+function idGen(){
+
+    let text = "";
+
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz12234567890"
+
+    const gen = () =>{
+
+        text += letters.charAt(Math.floor(Math.random() * letters.length))
+
+    }
+
+    var i = 0
+    while(i !== 5){
+
+        gen()
+
+        i++
+
+    }
+
+    return text
+
+}
+
+app.get("/", (req, res) =>{
+
+    res.sendFile(__dirname + "/index.html")
+
+})
+
+app.get("/redirect/:id", async(req, res) => {
+
+    const id = req.params.id
+
+    if(!id) return res.redirect("/")
+
+    const link = await db.get(id)
+
+    if(!link) return res.redirect("/")
+
+    res.redirect(link)
+
+})
+
+app.get("/create", async(req, res) => {
+
+    const url = req.query.url
+
+    if(!url) return res.send({"error": "No URL Param found"})
+
+    let id = idGen()
+
+    let thereIs = true
+
+    while(thereIs == false){
+
+        const check = await db.has(id)  
+
+        if(check) return id = idGen()
+
+        thereIs = false
+
+    }
+
+    await db.set(id, url)
+
+    res.send({"link": `http://localhost:8080/redirect/${id}`})
+
+})
+
+}catch(e) {console.log(e)}
